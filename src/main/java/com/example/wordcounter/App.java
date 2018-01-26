c8ffa79b77bd102c595559ccbb9dec98126f6803
qasiwe <qasiwe@gmail.com>
2018-01-26 13:37:35 +0600

    Firts commit

--- src/main/java/com/example/wordcounter/App.java
+++ src/main/java/com/example/wordcounter/App.java
@@ -2,12 +2,19 @@ package com.example.wordcounter;
 
 public class App 
 {
+	
+	
+	
+	
     public static void main( String[] args )
     {
         System.out.println("Word Count Testing App!");
     }
     
     public static int wordcount(String s) {
-        return 0;
+		String trim = s.trim();
+		if (trim.isEmpty())
+		    return 0;
+		return trim.split("\\s+").length;
     }
 }
